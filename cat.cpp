831f6f36fed3edef788a5c3e327cf28c84821716
Parker M <99286695+p-maziarz@users.noreply.github.com>
2023-10-28 16:00:02 -0700

    Update cat.cpp

--- cat.cpp
+++ cat.cpp
@@ -1,5 +1,14 @@
-//
-// Created by parke on 10/28/2023.
-//
-
 #include "cat.hpp"
+
+cat::cat() {
+    cat_name = " ";
+    cat_weight = 0;
+    cat_price = 0.0;
+}
+
+cat::cat(string name, int weight, float price) {
+    cat_name = name;
+    cat_weight = weight;
+    cat_price = price;
+}
+
