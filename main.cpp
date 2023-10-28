ac53b0df6beeb3b1346868d717e1abc09fdab206
Parker M <99286695+p-maziarz@users.noreply.github.com>
2023-10-28 15:58:26 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,14 @@ using namespace std;
 //Calculate license fees
 //Rate can be set to $2/lb for a dog, $1.50/lb for a cat, output the name of the pet and the fee
 int main() {
-
+    vector<int> pvec;
+    float total_fee = 0.0;
+    for (int i = 0; i < pvec.size(); i++)
+    {
+        cout << pvec[i]->GetName() << ' ' << pvec[i]->CalculateFee() << endl;
+        total_fee = total_fee + pvec[i]->CalculateFee();
+    }
+    cout << "total licensing fee = " << total_fee << endl;
     return 0;
+    //Create three pets in main
 }
