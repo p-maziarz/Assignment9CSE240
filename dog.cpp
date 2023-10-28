31f569eb0bce7bf1be62f97c0abfc851b5701e2e
Parker M <99286695+p-maziarz@users.noreply.github.com>
2023-10-28 15:59:29 -0700

    Update dog.cpp

--- dog.cpp
+++ dog.cpp
@@ -1,5 +1,13 @@
-//
-// Created by parke on 10/28/2023.
-//
-
 #include "dog.hpp"
+
+dog::dog() {
+    dog_name = " ";
+    dog_weight = 0;
+    dog_price = 0.0;
+}
+
+dog::dog(string name, int weight, float price) {
+    dog_name = name;
+    dog_weight = weight;
+    dog_price = price;
+}
