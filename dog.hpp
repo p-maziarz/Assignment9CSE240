80abcd8a182cca0674f5f928be5760a2d2941c30
Parker M <99286695+p-maziarz@users.noreply.github.com>
2023-10-28 15:59:44 -0700

    Update dog.hpp

--- dog.hpp
+++ dog.hpp
@@ -11,16 +11,29 @@ class dog : public pet {
 public:
     dog();
     dog(string name, int weight, float price);
-    void SetName(string name) {dog_name = name;}
-    string GetName() {return dog_name;}
-    void SetPrice(float price) {dog_price = price;}
-    float GetPrice() {return dog_price;}
+    void SetName(string name) {
+        dog_name = name;
+    }
+    string GetName() {
+        return dog_name;
+    }
+    void SetWeight(int weight) {
+        dog_weight = weight;
+    }
+    int GetWeight() {
+        return dog_weight;
+    }
+    void SetPrice(float price) {
+        dog_price = price;
+    }
+    float GetPrice() {
+        return dog_price;
+    }
 private:
     string dog_name;
-    int weight;
-    float dog_price = (weight * 2);
+    int dog_weight;
+    float dog_price = dog_weight*2;
 
 };
 
-
-#endif //ASSIGNMENT9_DOG_HPP
+#endif //dog_hpp
