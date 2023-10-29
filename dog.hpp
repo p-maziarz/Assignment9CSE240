b650c1fe45f96e1be226f2daa4e7b4758ada35e4
Parker M <99286695+p-maziarz@users.noreply.github.com>
2023-10-29 10:25:22 -0700

    Update dog.hpp

--- dog.hpp
+++ dog.hpp
@@ -2,40 +2,20 @@
 #define dog_hpp
 
 #include <string>
-#include <iostream>
 #include "pet.hpp"
 
 using namespace std;
 
 class dog : public pet {
 public:
-    dog();
-    dog(string name, int weight, float price);
-    void SetName(string name) {
-        dog_name = name;
-    }
-    string GetName() {
-        return dog_name;
-    }
-    void SetWeight(int weight) {
-        dog_weight = weight;
-    }
-    int GetWeight() {
-        return dog_weight;
-    }
-    void SetPrice(float price) {
-        dog_price = price;
-    }
-    float GetPrice() {
-        return dog_price;
+    dog(string name, int weight, float price)
+            : pet(name, weight, price)
+    {}
+    virtual float CalculateFee()
+    {
+        return licensing_rate * GetWeight();
     }
     static float licensing_rate;
-private:
-    string dog_name;
-    int dog_weight;
-    float dog_price;
 };
 
-float dog::licensing_rate = 2.0; //sets the rate for dogs at $2 a pound
-
-#endif //dog_hpp
+#endif
