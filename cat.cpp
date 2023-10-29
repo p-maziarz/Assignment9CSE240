859b33525c70c52fb190336b1425cbce49978838
Parker M <99286695+p-maziarz@users.noreply.github.com>
2023-10-29 10:24:54 -0700

    Update cat.cpp

--- cat.cpp
+++ cat.cpp
@@ -1,15 +1,4 @@
 #include "cat.hpp"
-//includes data for cat
+#include <string>
 
-cat::cat() {
-    cat_name = " ";
-    cat_weight = 0;
-    cat_price = 0.0;
-}
-
-cat::cat(string name, int weight, float price) {
-    cat_name = name;
-    cat_weight = weight;
-    cat_price = price;
-}
-
+float cat::licensing_rate = 1.50;
