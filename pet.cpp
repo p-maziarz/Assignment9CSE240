020cf103f740cacb81bb690b2de822246fe8c838
Parker M <99286695+p-maziarz@users.noreply.github.com>
2023-10-29 10:25:59 -0700

    Update pet.cpp

--- pet.cpp
+++ pet.cpp
@@ -1,14 +1,2 @@
 #include "pet.hpp"
-//includes the data from the pet file
 
-pet::pet() {
-    pet_name = " ";
-    pet_weight = 0;
-    pet_price = 0.0;
-}
-
-pet::pet(string name, int weight, float price) {
-    pet_name = name;
-    pet_weight = weight;
-    pet_price = price;
-}
