7343198550231b967037e550f1f7b39cb56ee8bf
Parker M <99286695+p-maziarz@users.noreply.github.com>
2023-10-29 10:25:06 -0700

    Update cat.hpp

--- cat.hpp
+++ cat.hpp
@@ -8,33 +8,14 @@ using namespace std;
 
 class cat : public pet {
 public:
-    cat();
-    cat(string name, int weight, float price);
-    void SetName(string name) {
-        cat_name = name;
-    }
-    string GetName() {
-        return cat_name;
-    }
-    void SetWeight(int weight) {
-        cat_weight = weight;
-    }
-    int GetWeight() {
-        return cat_weight;
-    }
-    void SetPrice(float price) {
-        cat_price = price;
-    }
-    float GetPrice() {
-        return cat_price;
+    cat(string name, int weight, float price)
+            : pet(name, weight, price)
+    {}
+    virtual float CalculateFee()
+    {
+        return licensing_rate * GetWeight();
     }
     static float licensing_rate;
-private:
-    string cat_name;
-    int cat_weight;
-    float cat_price = cat_weight;
 };
 
-float cat::licensing_rate = 1.50;
-
-#endif //cat_hpp
+#endif
